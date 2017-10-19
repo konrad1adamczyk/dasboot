ba51bff4fe5557d7e2c9421682129dca4649b3f2
Konrad <konrad1adamczyk@gmail.com>
2017-10-19 13:07:14 +0200

    lecimy

--- src/main/java/com/boot/model/Shipwreck.java
+++ src/main/java/com/boot/model/Shipwreck.java
@@ -1,14 +1,21 @@
 package com.boot.model;
 
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Shipwreck {
-	Long id;
-	String name;
-	String description;
-	String condition;
-	Integer depth;
-	Double latitude;
-	Double longitude;
-	Integer yearDiscovered;
+
+	private Long id;
+	private String name;
+	private String description;
+	private String condition;
+	private Integer depth;
+	private Double latitude;
+	private Double longitude;
+	private Integer yearDiscovered;
 
 	public Shipwreck() { }
 
@@ -23,67 +30,67 @@ public class Shipwreck {
 		this.yearDiscovered = yearDiscovered;
 	}
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public String getCondition() {
-		return condition;
-	}
-
-	public void setCondition(String condition) {
-		this.condition = condition;
-	}
-
-	public Integer getDepth() {
-		return depth;
-	}
-
-	public void setDepth(Integer depth) {
-		this.depth = depth;
-	}
-
-	public Double getLatitude() {
-		return latitude;
-	}
-
-	public void setLatitude(Double latitude) {
-		this.latitude = latitude;
-	}
-
-	public Double getLongitude() {
-		return longitude;
-	}
-
-	public void setLongitude(Double longitude) {
-		this.longitude = longitude;
-	}
-
-	public Integer getYearDiscovered() {
-		return yearDiscovered;
-	}
-
-	public void setYearDiscovered(Integer yearDiscovered) {
-		this.yearDiscovered = yearDiscovered;
-	}
+//	public Long getId() {
+//		return id;
+//	}
+//
+//	public void setId(Long id) {
+//		this.id = id;
+//	}
+//
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+//
+//	public String getDescription() {
+//		return description;
+//	}
+//
+//	public void setDescription(String description) {
+//		this.description = description;
+//	}
+//
+//	public String getCondition() {
+//		return condition;
+//	}
+//
+//	public void setCondition(String condition) {
+//		this.condition = condition;
+//	}
+//
+//	public Integer getDepth() {
+//		return depth;
+//	}
+//
+//	public void setDepth(Integer depth) {
+//		this.depth = depth;
+//	}
+//
+//	public Double getLatitude() {
+//		return latitude;
+//	}
+//
+//	public void setLatitude(Double latitude) {
+//		this.latitude = latitude;
+//	}
+//
+//	public Double getLongitude() {
+//		return longitude;
+//	}
+//
+//	public void setLongitude(Double longitude) {
+//		this.longitude = longitude;
+//	}
+//
+//	public Integer getYearDiscovered() {
+//		return yearDiscovered;
+//	}
+//
+//	public void setYearDiscovered(Integer yearDiscovered) {
+//		this.yearDiscovered = yearDiscovered;
+//	}
 }
